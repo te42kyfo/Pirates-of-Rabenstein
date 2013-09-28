6152db23cabe0c1b3ea7dfc5d4da24f44a89e257
Christopher Bross <christopher.bross@fau.de>
2013-09-28 18:55:08 +0200

    fixed missing multi_array.hpp

--- src/Game_simulation.cpp
+++ src/Game_simulation.cpp
@@ -16,7 +16,7 @@ You should have received a copy of the GNU Affero General Public License along
 with this program.  If not, see <http://www.gnu.org/licenses/>. */
 
 #include "Game.hpp"
-#include <multi_array.hpp>
+//#include <multi_array.hpp>
 #include <iostream>
 using namespace std;
 
